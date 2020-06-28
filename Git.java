850b8041a212ca69132c8c16bdbbbd541b116930
sametyaprak <sametyaprak@gmail.com>
2020-06-28 19:12:39 +0200

    ilk gonderim

---
+++ Git.java
@@ -0,0 +1,20 @@
+package samet1;
+
+public class Git {
+
+	public static void main(String[] args) {
+		
+		System.out.println("basladik");
+		System.out.println("basladik");
+		System.out.println("basladik");
+		System.out.println("basladik");
+		System.out.println("basladik");
+		System.out.println("basladik");
+		System.out.println("basladik");
+		System.out.println("basladik");
+		System.out.println("basladik");
+		
+
+	}
+
+}
